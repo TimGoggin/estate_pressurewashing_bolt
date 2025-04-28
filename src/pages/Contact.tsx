d6aa967c34b5adc07f1ae215c91c30657d519b90
TimGoggin <82612384+TimGoggin@users.noreply.github.com>
2025-04-28 17:56:02 -0400

    Update Contact.tsx
    blank space

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -14,7 +14,7 @@ export default function Contact() {
     <div
       className={`min-h-screen ${darkMode ? "bg-gray-900 text-white" : "bg-white text-gray-800"}`}
     >
-      <div className="relative pt-32 pb-20">
+      <div className="relative pt-32 pb-20 ">
         <div className="container mx-auto px-6">
           <h1 className="text-4xl md:text-5xl font-bold mb-6">
             Contact Us
