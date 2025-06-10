51cb9871f14cc3e72fe6128bfc9f66c6e5702735
TimGoggin <82612384+TimGoggin@users.noreply.github.com>
2025-06-10 15:49:42 +0000

    fixed routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import App from './App'
-import About from './pages/About'
-import Services from './pages/Services';
-import Gallery from './pages/Gallery';
-import Contact from './pages/Contact';
-import Layout from './components/Layout'
-import Accessibility from './pages/Accessibility';
-import Privacy from './pages/Privacy';
-import Terms from './pages/Terms';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import Layout from "./components/Layout";
+import Accessibility from "./pages/Accessibility";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<App />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about-us" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact-us" element={<Contact />} />
           <Route path="/accessibility-statement" element={<Accessibility />} />
           <Route path="/privacy-policy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
@@ -29,4 +29,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </Routes>
     </Router>
   </React.StrictMode>,
-)
+);
